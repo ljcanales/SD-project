0a11ecffb8ea66053c990d323b8ed7582e1f725c
ljcanales <ljcanales1404@gmail.com>
2022-06-11 00:03:20 -0300

    Enable MongoRepo

--- src/main/java/com/sd/apirest/ApirestApplication.java
+++ src/main/java/com/sd/apirest/ApirestApplication.java
@@ -2,8 +2,10 @@ package com.sd.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ApirestApplication {
 
 	public static void main(String[] args) {
